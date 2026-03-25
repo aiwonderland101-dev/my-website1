486df0938f0fb433fd670f5aca15eb07b438485f
aiwonderland101-dev <aiwonderland101@gmail.com>
2026-03-25 17:14:17 +0000

    updated the index.ts file to export the PlayCanvasEngine component from the correct path.

--- packages/unreal-wonder-build/index.ts
+++ packages/unreal-wonder-build/index.ts
@@ -1,2 +1,2 @@
-export { default as PlayCanvasEngine } from './PlayCanvasEngine';
+export { default as PlayCanvasEngine } from './components/PlayCanvasEngine.tsx';
 export { UnrealWonderBuildPage } from './src/UnrealWonderBuildPage';
